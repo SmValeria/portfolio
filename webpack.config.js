dfcbe317b1ac01f342ecc5655adf5af445b330cf
Valeria Smirnova <valeriasmyrnova@gmail.com>
2019-03-20 16:17:13 +0200

    footer adaptive

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = 'smvaleria.github.io/portfolio/';
+  const publicPath = './';
 
   const pcss = {
     test: /\.(p|post|)css$/,
