61e97e75ac8fa5f521b75f129e669ccb4b68518c
Valeria Smirnova <valeriasmyrnova@gmail.com>
2019-03-26 13:28:31 +0200

    reviews slider

---
+++ src/scripts/reviews.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Flickity from 'vue-flickity';
+
+new Vue({
+    el: "#reviews-component",
+    template: "#reviews-container",
+    components: {
+        Flickity
+    },
+
+    data() {
+        return {
+            reviews: [],
+            flickityOptions: {
+                prevNextButtons: false,
+                pageDots: false,
+                wrapAround: true,
+                groupCells: window.screen.width > 480 ? 2 : 1,
+                resize: true,
+            }
+        }
+    },
+    methods: {
+        next() {
+            this.$refs.flickity.next();
+        },
+
+        previous() {
+            this.$refs.flickity.previous();
+        },
+        makeArrWithRequiredPathImages(data) {
+            return data.map(item => {
+                const requiredPic = require(`../images/content/${item.photo}`);
+                item.photo = 'url( \"' + requiredPic + '\")';
+                return item;
+            });
+        },
+    },
+    created() {
+        const data = require("../data/reviews.json");
+        this.reviews = this.makeArrWithRequiredPathImages(data);
+    }
+});
