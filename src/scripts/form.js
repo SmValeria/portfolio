8476837e8e445866d510ef387b9e3355a601b5c2
Valeria Smirnova <valeriasmyrnova@gmail.com>
2019-03-28 22:45:08 +0200

    style form inputs

---
+++ src/scripts/form.js
@@ -0,0 +1,62 @@
+const form = document.querySelector('.form__container');
+const inputs = form.querySelectorAll('.form__input');
+const submitButton = form.querySelector('.form__submit');
+
+submitButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    if (!checkForm()) {
+        return;
+    }
+
+
+});
+
+inputs.forEach(input => {
+    const inputWrapper = input.closest('.form__block');
+    input.addEventListener('focus', function () {
+        inputWrapper.classList.add('active');
+        inputWrapper.querySelector('.form__error').classList.add('hidden');
+    });
+    input.addEventListener('blur', function () {
+        if (!input.value) {
+            inputWrapper.classList.remove('active');
+        }
+
+    });
+});
+
+function checkForm() {
+    let validity = true;
+
+    if (!checkInput(form.elements.name)) {
+        validity = false;
+    }
+
+    if (!checkInput(form.elements.email)) {
+        validity = false;
+    }
+
+    if (!checkInput(form.elements.message)) {
+        validity = false;
+    }
+
+
+    return validity
+}
+
+
+function checkInput(input) {
+    const inputWrapper = input.closest('.form__block');
+    const errorWrapper = inputWrapper.querySelector('.form__error');
+    if (!input.checkValidity()) {
+        errorWrapper.textContent = input.validationMessage;
+        errorWrapper.classList.remove('hidden');
+        console.log(errorWrapper);
+        return false
+    } else {
+        errorWrapper.textContent = '';
+        return true
+    }
+
+}
